815ec34bdbef8d562bba4d2bf9a2da432cc6bdf3
nishikanth <32055268+nishikanth@users.noreply.github.com>
2020-05-14 18:56:55 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,4 @@
-ibmcloud_api_key = ""
-iaas_classic_username = "SL2083704"
+ibmcloud_api_key = "xkYslsB-A37D1OrrVKRgMseyTqGsFthqklYhcdCdAQsl"
+iaas_classic_username = "SL2084148"
 iaas_classic_api_key = "b437c6a8f90e4889c736673a3100a4a63ffbb9be58cc7d54e785f9dd95620c11"
 region = "us-south"
